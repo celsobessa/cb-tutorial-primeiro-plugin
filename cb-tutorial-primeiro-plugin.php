a744600e8dbaf961471d6f5c61a71ea56fe18062
Celso Bessa <celso@celsobessa.com.br>
2018-05-26 00:33:37 -0300

    WIP20180526a

--- cb-tutorial-primeiro-plugin.php
+++ cb-tutorial-primeiro-plugin.php
@@ -26,6 +26,166 @@
  */
 
 // If this file is called directly, abort.
+// se o arquivo for chamado diretamente, aborte.
 if ( ! defined( 'WPINC' ) ) {
 	die;
+}
+
+// useful constants: plugin's version and path.
+// constantes úteis: versão e caminho do plugin.
+define( 'CBTPL_PLUGIN_VERSION', '0.1.0' );
+define( 'CBTPL_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+if ( is_admin() ) {
+
+	/**
+	 * Bloqueia acesso ao painel condicionalmente
+	 *
+	 * Bloqueia acesso ao painel se não for administrador ou editor
+	 *
+	 * @since  0.1.0
+	 * @return void Markup HTML para o aviso.
+	 */
+	function cbtpl_blockusers_init() {
+
+		// verifica se o usuário pode usar manage_options.
+		// veja Roles e Capabilities em https://codex.wordpress.org/Roles_and_Capabilities .
+		if ( ! current_user_can( 'edit_others_posts' ) ) {
+			wp_safe_redirect( home_url() );
+			exit;
+		}
+	}
+	add_action( 'init', 'cbtpl_blockusers_init' );
+
+	/**
+	 * Gerencia os avisos para o plugin.
+	 *
+	 * Gerencia os avisos para o plugin.
+	 *
+	 * @since  0.1.0
+	 * @return void Markup HTML para o aviso.
+	 */
+	function cbtpl_notices() {
+		?>
+		<div class="notice notice-success is-dismissible">
+			<p><?php echo 'A vida é muito curta para não comer pão de queijo do Na Venda Pão de Queijaria'; ?></p>
+		</div>
+	<?php
+	}
+	add_action( 'admin_notices', 'cbtpl_notices' );
+
+}
+
+if ( ! is_admin() ) {
+
+	// Remove a barra de admin na área pública.
+	add_filter( 'show_admin_bar', '__return_false' );
+
+	// remove diversos ítens do wp_head.
+	remove_action( 'wp_head', 'wlwmanifest_link' );
+	remove_action( 'wp_head', 'rsd_link' );
+	remove_action( 'wp_head', 'wp_generator' );
+	remove_action( 'wp_head', 'start_post_rel_link' );
+	remove_action( 'wp_head', 'index_rel_link' );
+
+	// note o uso do terceiro parâmetro, prioridade.
+	// via de grega, remoções de hooks devem usar a mesma prioridade.
+	remove_action( 'wp_head', 'feed_links_extra', 3 );
+	remove_action( 'wp_head', 'feed_links', 2 );
+
+	// note o uso do quarto parâmetro, prioridade, número de parâmetros da função hook.
+	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+
+	/**
+	 * Filtra as classes do elemento body
+	 *
+	 * Filtra condicionalmente as as classes do elemento body conforme a seção do site
+	 *
+	 * @since  0.1.0
+	 * @param array $classes As classes do elemento body.
+	 * @return array $classes As classes do elemento body, filtradas.
+	 */
+	function cbtpl_body_class( $classes ) {
+
+		// Adiciona uma classe geral para o plugin.
+		$classes[] = 'cbtpl';
+
+		// Adiciona uma classe se for page, se for post, se for archive ou se for home.
+		if ( is_page() ) {
+			$classes[] = 'cbtpl-page';
+		} else if ( is_single() ) {
+			$classes[] = 'cbtpl-single';
+		} else if ( is_archive() ) {
+			$classes[] = 'cbtpl-archive';
+		} else if ( is_home() ) {
+			$classes[] = 'cbtpl-home';
+		}
+
+		// Retorna as classes filtradas.
+		return $classes;
+
+	}
+	add_filter( 'body_class','cbtpl_body_class' );
+
+
+	/**
+	 * Adiciona um iframe com o mapa japu ao final do conteúdo.
+	 *
+	 * Adiciona um iframe com o mapa japu ao final do conteúdo de posts and pages
+	 *
+	 * @since 0.1.0
+	 * @param string $content O conteúdo do post.
+	 * @return string $content O conteúdo do post, filtrado, com o iframe ao final.
+	 */
+	function cbtpl_add_japu_map( $content ) {
+		global $post;
+
+		// se o tipo de post não for post ou page, retorna $content sem modificar e sai da função.
+		if ( 'post' !== $post->post_type && 'page' !== $post->post_type ) {
+			return $content;
+		}
+
+		// cria uma string $markup para o html gerado
+		$markup = '<div class="japumap-wrapper">';
+		$markup .= '<iframe src="https://embed.japuapp.com.br/" sandbox="allow-same-origin allow-scripts allow-forms" style="border:none;" class="japumap-iframe"></iframe>';
+		$markup .= '<div class="japumap-bottom-bar">';
+		$markup .= '<p class="japumap-credits">';
+		$markup .= 'fonte: <a href="https://www.japuapp.com.br">Japu - Rotas das Vertentes</a>';
+		$markup .= '<a id="japumap-toggle-full-screen"><span class="return-text">disable fullscreen and return to our site</span><span class="toggle-full-screen-text">tela cheia</span></a>';
+		$markup .= '</p>';
+		$markup .= '</div>';
+		$markup .= '</div>';
+
+		// adiciona $markup após $content
+		$content .= $markup;
+
+		// retorn $content
+		return $content;
+	}
+
+	// prioridade alta para ser usada após outros plugins também filtrarem o conteúdo
+	add_filter( 'the_content', 'cbtpl_add_japu_map', 100 );
+
+	/**
+	 * Adiciona os estilos e scripts customizados.
+	 *
+	 * Adiciona os estilos e scripts customizados de nosso plugin
+	 *
+	 * @since  0.1.0
+	 * @return void
+	 */
+	function cbtpl_ativos_publicos() {
+
+		// Registra e coloca na fila nosso estilo customizado.
+		wp_register_style( 'cbtpl-style', CBTPL_PLUGIN_PATH . '/includes/styles/cbtpl-style.css' , array(), CBTPL_PLUGIN_VERSION, 'all' );
+		wp_enqueue_style( 'cbtpl-style' );
+
+		// Registra e coloca na fila nosso script customizado.
+		wp_register_script( 'cbtpl-script', CBTPL_PLUGIN_PATH . '/includes/js/cbtpl-script.js' , array(), CBTPL_PLUGIN_VERSION, true );
+		wp_enqueue_script( 'cbtpl-script' );
+	}
+	add_action( 'wp_enqueue_scripts', 'cbtpl_ativos_publicos' );
+
 }
